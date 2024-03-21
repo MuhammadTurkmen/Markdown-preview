b66872bad142b3fe35a722e689930e7c4add587d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-21 10:46:55 +0430

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 function App() {
+  const [markdown, setMarkdown] = useState
   return <h2>markdown preview starter</h2>
 }
 
