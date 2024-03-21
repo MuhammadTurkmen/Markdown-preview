d1b0e3edc3980e74c6c1185da2388911a1abf6c1
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-21 12:07:45 +0430

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <section className="markdown">
       <textarea className='input' value={markdown} onChange={(e) => setMarkdown(e.target.value)}></textarea>
       <article className='result'>
+        
         {markdown}
+        
       </article>
     </section>
   </main>
